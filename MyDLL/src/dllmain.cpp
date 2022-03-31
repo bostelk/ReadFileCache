7047db884fd78194314873698e2dc07903447e93
Kyle Bostelmann <kyle@bostelk.ca>
2022-03-31 07:49:05 -0300

    Replace distormx with detours.
    Rename target DLL with architecture.

--- MyDLL/src/dllmain.cpp
+++ MyDLL/src/dllmain.cpp
@@ -1,6 +1,5 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
-#include <distormx.h>
 #include <string>
 #include "sys.h"
 #include "logfile.h"
@@ -8,106 +7,43 @@
 #include "readfiletrace.h"
 #include "readfilecache.h"
 #include "readfiledirectstorage.h"
-
-#define is_aligned(POINTER, BYTE_COUNT) \
-    (((uintptr_t)(const void *)(POINTER)) % (BYTE_COUNT) == 0)
-
-#if defined(_UNICODE)
-#define _T(x) L ##x
-#else
-#define _T(x) x
-#endif
-
-BOOL IsReadFileHooked()
-{
-    return ReadFileOriginal != NULL;
-}
-
-int HookReadFile()
-{
-    if (IsReadFileHooked())
-    {
-        AppendLog("Skip because hooked");
-        return 1;
-    }
-
-    AppendLog("Load Kernel32");
-    HMODULE hKernel32 = LoadLibrary(_T("Kernel32.dll"));
-    if (hKernel32 == NULL)
-    {
-        AppendLog("Failed to load module");
-        return 1;
-    }
-
-    AppendLog("Find ReadFile");
-    ULONG_PTR pReadFile = (ULONG_PTR)GetProcAddress(hKernel32, "ReadFile");
-    if (pReadFile == NULL)
-    {
-        AppendLog("Failed to find function");
-        return 1;
-    }
-
-    AppendLog("Hook");
-    if (!distormx_hook((void**)&pReadFile, (void*)&ReadFileDirectStorage))
-    {
-        AppendLog("Failed Hook");
-        return 1;
-    }
-
-    ReadFileOriginal = (BOOL(WINAPI*)(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED)) pReadFile;
-    return 0;
-}
-
-int UnhookReadFile()
-{
-    if (!IsReadFileHooked())
-    {
-        AppendLog("Skip because not hooked");
-        return 1;
-    }
-
-    AppendLog("Unhook");
-    distormx_unhook((void*)&ReadFileOriginal);
-    distormx_destroy();
-
-    ReadFileOriginal = NULL;
-
-    return 0;
-}
+#include <detours.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
+                       DWORD  dwReason,
                        LPVOID lpReserved
                      )
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    {
+    if (DetourIsHelperProcess()) {
+        return TRUE;
+    }
+
+    if (dwReason == DLL_PROCESS_ATTACH) {
         LogInit(hModule);
         AppendLog("Process Attach");
 
-        //HookReadFile();
-        break;
+        DetourRestoreAfterWith();
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)ReadFileOriginal, (void*)&ReadFileTrace);
+        LONG error = DetourTransactionCommit();
+        if (error == NO_ERROR) 
+        {
+            AppendLog("MyDll" + std::string(DETOURS_STRINGIFY(DETOURS_BITS)) + ".dll: Detoured ReadFile().");
+        }
+        else 
+        {
+            AppendLog("MyDll" + std::string(DETOURS_STRINGIFY(DETOURS_BITS)) + ".dll: Error detouring ReadFile(): " + std::to_string(error));
+        }
     }
-    case DLL_THREAD_ATTACH:
-    {
-        AppendLog("Thread Attach");
-        HookReadFile();
-        break;
-    }
-    case DLL_THREAD_DETACH:
-    {
-        AppendLog("Thread Detach");
-        //UnhookReadFile();
-        break;
-    }
-    case DLL_PROCESS_DETACH:
-    {
+    else if (dwReason == DLL_PROCESS_DETACH) {
         AppendLog("Process Detach");
-        //UnhookReadFile();
-        break;
-    }
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)ReadFileOriginal, (void*)&ReadFileTrace);
+        DetourTransactionCommit();
     }
     return TRUE;
 }
--- MyDLL/src/readfileoriginal.cpp
+++ MyDLL/src/readfileoriginal.cpp
@@ -1,4 +1,4 @@
 #include "pch.h"
 #include "readfileoriginal.h"
 
-BOOL(WINAPI* ReadFileOriginal)(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped) = NULL;
+BOOL(WINAPI* ReadFileOriginal)(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped) = ReadFile;
